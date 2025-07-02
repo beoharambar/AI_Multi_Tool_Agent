f538b90ecb7ba8884062b4ab8a0dd0fd42f934fc
Ambar Beohar <120037855+beoharambar@users.noreply.github.com>
2025-07-02 17:31:11 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+GROQ_API_KEY  = "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"  # Replace with your OpenAI key
+SERPAPI_API_KEY = "1ba8d472dfd6c9ec1eed4e17d4e08fbbdc20b3a554f88442231d7e5229ed3548"  # Optional: For live Google Search
